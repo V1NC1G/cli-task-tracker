92003ce7e4dc4fb257094ad27af9729ddc1e3e0b
vinci <74002397+V1NC1G@users.noreply.github.com>
2024-08-15 18:01:36 +0800

    add functions to mark task status

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ class TaskManager:
 
     def update_task(self, id, description):
         task = self.get_task_by_id(id)
-        print(task)
         task["description"] = description
         print(f"Task (ID: {id}) updated")
         return
@@ -44,6 +43,24 @@ class TaskManager:
                 return task
         return None
 
+    def mark_todo(self, id):
+        task = self.get_task_by_id(id)
+        task["status"] = "todo"
+        print(f"Marked task (ID: {id}) as TODO.")
+        return
+
+    def mark_in_progress(self, id):
+        task = self.get_task_by_id(id)
+        task["status"] = "in progress"
+        print(f"Marked task (ID: {id}) as IN PROGRESS.")
+        return
+
+    def mark_done(self, id):
+        task = self.get_task_by_id(id)
+        task["status"] = "done"
+        print(f"Marked task (ID: {id}) as DONE.")
+        return
+
     def list_task(self):
         for task_item in self.tasks:
             print("\n")
@@ -51,6 +68,36 @@ class TaskManager:
             print(f"Task: {task_item['description']}")
             print(f"Status: {task_item['status'].upper()}")
             print("\n")
+        return
+
+    def list_todo(self):
+        for task_item in self.tasks:
+            if task_item["status"] == "todo":
+                print("\n")
+                print(f"ID: {task_item['id']}")
+                print(f"Task: {task_item['description']}")
+                print(f"Status: {task_item['status'].upper()}")
+                print("\n")
+        return
+
+    def list_in_progress(self):
+        for task_item in self.tasks:
+            if task_item["status"] == "in progress":
+                print("\n")
+                print(f"ID: {task_item['id']}")
+                print(f"Task: {task_item['description']}")
+                print(f"Status: {task_item['status'].upper()}")
+                print("\n")
+        return
+
+    def list_done(self):
+        for task_item in self.tasks:
+            if task_item["status"] == "done":
+                print("\n")
+                print(f"ID: {task_item['id']}")
+                print(f"Task: {task_item['description']}")
+                print(f"Status: {task_item['status'].upper()}")
+                print("\n")
         return
 
 
@@ -63,15 +110,40 @@ def main():
         if action == "add":
             description = " ".join(command.split()[1:])
             task_manager.add_task(description)
+
         elif action == "update":
             id = int(command.split()[1])
             description = " ".join(command.split()[2:])
             task_manager.update_task(id, description)
+
         elif action == "delete":
             id = int(command.split()[1])
             task_manager.delete_task(id)
+
+        elif action == "mark-todo":
+            id = int(command.split()[1])
+            task_manager.mark_todo(id)
+
+        elif action == "mark-in-progress":
+            id = int(command.split()[1])
+            task_manager.mark_in_progress(id)
+
+        elif action == "mark-done":
+            id = int(command.split()[1])
+            task_manager.mark_done(id)
+
         elif action == "list":
             task_manager.list_task()
+
+        elif action == "list-todo":
+            task_manager.list_todo()
+
+        elif action == "list-in-progress":
+            task_manager.list_in_progress()
+
+        elif action == "list-done":
+            task_manager.list_done()
+
         elif action == "end":
             print("Ending task manager program...")
             return False
